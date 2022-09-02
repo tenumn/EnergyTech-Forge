df7d84f8de3476e1f33108aeedd5b90e8ae4832f
Tenumn <tenumn@outlook.com>
2022-09-03 06:47:19 +0800

    添加 金属板

--- src/main/java/tenumn/energytech/ModItems.java
+++ src/main/java/tenumn/energytech/ModItems.java
@@ -4,10 +4,15 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import tenumn.energytech.item.ItemBase;
 import tenumn.energytech.item.ItemEnergyCard;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EnergyTech.ID);
 
     public static final RegistryObject<ItemEnergyCard> energy_card = ITEMS.register("energy_card", () -> new ItemEnergyCard(new Item.Properties()));
+    public static final RegistryObject<ItemBase> copper_plate = ITEMS.register("copper_plate",() -> new ItemBase(new Item.Properties()));
+    public static final RegistryObject<ItemBase> iron_plate = ITEMS.register("iron_plate",() -> new ItemBase(new Item.Properties()));
+    public static final RegistryObject<ItemBase> steel_plate = ITEMS.register("steel_plate",() -> new ItemBase(new Item.Properties()));
+    public static final RegistryObject<ItemBase> gold_plate = ITEMS.register("gold_plate",() -> new ItemBase(new Item.Properties()));
 }
